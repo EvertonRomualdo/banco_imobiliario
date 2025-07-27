55e0f7c334c4c20b260b5864e45b019eb6d145d3
Everton Daniel <evertonrdaniel@gmail.com>
2025-07-27 00:43:18 -0300

    ADD: .gitignore adicionado e imports do Main.hs atualizados

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -1,6 +1,9 @@
 module Main where
 
 import qualified MyLib (someFunc)
+import qualified Game.Player as Pl
+import qualified Game.BoardHouse as Bh 
+import qualified Game.Board as Gb
 
 main :: IO ()
 main = do
